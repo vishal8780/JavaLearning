453894f43fe00ec3b6005c071856c8e54d19e315
vishal8780 <vishalkataka@gmail.com>
2025-04-03 06:35:04 -0400

    MinMax

---
+++ src/Method/MinMaxOfFourNo.java
@@ -0,0 +1,16 @@
+package Method;
+
+import java.util.Scanner;
+
+public class MinMaxOfFourNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        double d = sc.nextDouble();
+        System.out.println(Math.max(a,Math.max(b,Math.max(c,d))));
+        System.out.println(Math.min(a,Math.min(b,Math.min(c,d))));
+
+    }
+}
